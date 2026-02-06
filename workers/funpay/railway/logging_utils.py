1c14f15376e09519e9b5e9e6a3f7e86921642b65
megafartCc <csgotest2027@gmail.com>
2026-02-06 16:31:34 -0600

    Show FunPayAPI debug logs when offerSave debug enabled

--- workers/funpay/railway/logging_utils.py
+++ workers/funpay/railway/logging_utils.py
@@ -11,5 +11,8 @@ def configure_logging() -> logging.Logger:
         level=os.getenv("LOG_LEVEL", "INFO"),
         format=LOG_FORMAT,
     )
-    logging.getLogger("FunPayAPI").setLevel(logging.WARNING)
+    if os.getenv("FUNPAY_DEBUG_OFFER_SAVE"):
+        logging.getLogger("FunPayAPI").setLevel(logging.INFO)
+    else:
+        logging.getLogger("FunPayAPI").setLevel(logging.WARNING)
     return logging.getLogger("funpay.worker")
